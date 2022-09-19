ee81937ab53edfb7f6083aa244bb1da04a5c7d24
taegon <taegon1998@gmail.com>
2022-09-19 23:45:21 +0900

    first commit

---
+++ TumbleProject/src/main/java/com/TumbleProject/TumbleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.TumbleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumbleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TumbleProjectApplication.class, args);
+	}
+
+}
