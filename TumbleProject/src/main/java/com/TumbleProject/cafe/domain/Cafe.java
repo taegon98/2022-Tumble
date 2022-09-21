62298037ca0c9de18800db7fc51ae94d50d6ea43
im2sh <104557685+im2sh@users.noreply.github.com>
2022-09-21 21:24:27 +0900

    Seokhwan

--- TumbleProject/src/main/java/com/TumbleProject/cafe/domain/Cafe.java
+++ TumbleProject/src/main/java/com/TumbleProject/cafe/domain/Cafe.java
@@ -16,5 +16,7 @@ public class Cafe {
     private String name;
     private String address;
     private String phoneNum;
-    private Integer discount;
+    private String discount;
+    private String hour;
+    private String introduce;
 }
